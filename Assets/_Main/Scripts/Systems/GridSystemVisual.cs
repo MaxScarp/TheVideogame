8f2f9fc295c649c55f80304f88b83b9557bbab16
MaxScarp <massy1997.lavoro@gmail.com>
2023-09-08 15:35:01 +0200

    Development
    Beginning of the visual for GridSystem

---
+++ Assets/_Main/Scripts/Systems/GridSystemVisual.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GridSystemVisual : MonoBehaviour
+{
+    [SerializeField] private Transform gridObjectVisualPrefab;
+    [SerializeField] private Material[] gridObjectMaterialArray;
+
+
+}
